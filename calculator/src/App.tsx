0595a196a30b40c0bcae7ac341f2e70a14fd9e3b
Анастасия Трушева <a.a.trusheva@stud.mospolytech.ru>
2024-11-05 03:03:27 +0300

    Написана часть валидации ввода

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -8,10 +8,16 @@ function App() {
   
     const [displayValue, setDisplayValue] = useState<string>('');
     const [answ, setAnsw] = useState<string>('');
-    const [spanValue, setSpanValue] = useState<string>('');
+    const [spanValue, setSpanValue] = useState<string>('');//значения поля под полем ввода
+    const [leftBraketCounter, setLeftBraketCounter] = useState(0);//счетчик скобок
     
     const handleEqualClick = async () =>{
-      try {
+      if (leftBraketCounter!=0){
+        setSpanValue('Не все скобки закрыты');
+      }
+      else
+      {
+        try {
         // Отправка AJAX-запроса на сервер
         getAnsw(displayValue)
         .then(data => {
@@ -24,33 +30,74 @@ function App() {
         });
       } catch (error) {
         setSpanValue('Ошибка сервера');
-      }
+      }}
     }
   
 
     const handleButtonClick = (button) => {
-      switch (button) {
+      switch (button) {///валидация ввода
         case 'C':
-          setDisplayValue(displayValue.slice(0, -1));
+          if (displayValue.slice(displayValue.length - 1, displayValue.length)==="("){
+            setLeftBraketCounter(leftBraketCounter-1);
+            setDisplayValue(displayValue.slice(0, -1));
+          }
+          else if (displayValue.slice(displayValue.length - 1, displayValue.length)===")"){
+            setLeftBraketCounter(leftBraketCounter+1);
+            setDisplayValue(displayValue.slice(0, -1));
+          }
+          else{
+            setDisplayValue(displayValue.slice(0, -1));
+          }
           break;
         case 'CE':
           setDisplayValue("");
+          setLeftBraketCounter(0);
+          break;
+        case '-':  
+          if(displayValue === '' ||"/-×+√^".includes(displayValue.slice(displayValue.length - 1, displayValue.length)))
+          {
+            setDisplayValue(displayValue + "(" + button);
+            setLeftBraketCounter(leftBraketCounter+1);
+          break;
+          }
+          else 
+          {setDisplayValue(displayValue + button);}
           break;
         case '+':
-        case '-':
         case '√':
         case '^':
         case '×':
         case '/':
-          if ("/-×+√^".includes(displayValue.slice(displayValue.length - 1, displayValue.length))) {
-            break; // Do nothing if the last character is already an operator
-          } else if (displayValue === '' && button === "-") {
-            // Special case for negative sign at the beginning
-            setDisplayValue(displayValue + "(" + button);
-          } else {
+          if ("/-×+√^".includes(displayValue.slice(displayValue.length - 1, displayValue.length)))
+          { 
+             break; // Do nothing if the last character is already an operator
+          }  
+           else {
             setDisplayValue(displayValue + button);
           }
           break;
+
+        case '(':
+         if (displayValue.slice(displayValue.length - 1, displayValue.length)===")"){ }
+         else {
+          setDisplayValue(displayValue + button);
+          setLeftBraketCounter(leftBraketCounter+1);
+        }
+         break;
+        case ')':
+         if (displayValue.slice(displayValue.length - 1, displayValue.length)==="("){}
+         else {
+          setDisplayValue(displayValue + button);
+          
+          setLeftBraketCounter(leftBraketCounter-1);
+        }
+
+         break;       
+              
+             
+      
+          
+            
         default:
           // Default case: Add the button to the display value
           setDisplayValue(displayValue + button);
